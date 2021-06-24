2cbe62d45b24062df6b1dfd95404c1969682aee3
Seogi Kang <sgkang09@gmail.com>
2021-06-24 16:01:57 -0700

    add survey

--- insarinv/simulation.py
+++ insarinv/simulation.py
@@ -1,20 +1,20 @@
 from SimPEG.simulation import BaseSimulation 
-from SimPEG import utils, props
+from SimPEG import utils, props, Data
 import numpy as np
 from discretize import TensorMesh
 import scipy.sparse as sp
+from scipy.signal import medfilt
 
-
-class InSARSimulatino1D(BaseSimulation):
-    """docstring for InSARSimulatino1D"""
+class InSARSimulation1D(BaseSimulation):
+    """docstring for InSARSimulation1D"""
 
     Sske, SskeMap, SskeDeriv = props.Invertible(
-        "Elastic skeletion specific storage [m-1]", 
+        "Specific skeleton storage of clay (elastic) [m-1]", 
         default=6.39*1e-6
     )
 
     Sskv, SskvMap, SskvDeriv = props.Invertible(
-        "Virgin skeleton specfic storage [m-1]",
+        "Specific skeleton storage of clay (virgin or inelastic) [m-1]",
         default=3.5*1e-4
     )
 
@@ -40,9 +40,14 @@ class InSARSimulatino1D(BaseSimulation):
     )
     
     Sska, SskaMap, SskaDeriv = props.Invertible(
-        "Aquifer specific storage [m-1]", 
+        "Specific skeleton storage of aquifer (elastic)[m-1]", 
         default=1.19*1e-5
-    )    
+    )
+
+    Ssw, SswMap, SswDeriv = props.Invertible(
+        "Specific stroage of water [m-1]", 
+        default=1.5e-6
+    )        
 
     b_aquifer, b_aquiferMap, b_aquiferDeriv = props.Invertible(
         "Thickness of the aquifer [m]"
@@ -55,6 +60,10 @@ class InSARSimulatino1D(BaseSimulation):
 
     time_simulation = props.Array(
         "time (s)"
+    )
+
+    time_channels = props.Array(
+        "time_channels (s)"
     )
 
     def __init__(self, **kwargs):
@@ -83,6 +92,12 @@ class InSARSimulatino1D(BaseSimulation):
     @property
     def n_time(self):
         return self.time_simulation.size
+
+    @property
+    def time_mesh(self):
+        if getattr(self, "_time_mesh", None) is None:
+            self._time_mesh = TensorMesh([np.diff(self.time_simulation)], x0=[self.time_simulation[0]])
+        return self._time_mesh
 
     @property
     def L(self):
@@ -221,15 +236,15 @@ class InSARSimulatino1D(BaseSimulation):
         J = tmp[inds_tmp]
         data = np.ones(inds_tmp.sum())
         n = h.size
-        Pmin = sp.coo_matrix((data, (I, J)), shape=(n,n))
+        Pmin = sp.coo_matrix((data, (I, J)), shape=(n,n)).tocsr()
 
         data = np.ones(n)
         I = np.arange(n_time).repeat(nz)
         J = np.array([np.arange(nz) * n_time + ii for ii in range(n_time)]).ravel()
-        Psum = sp.coo_matrix((data, (I, J)), shape=(n_time,n))
+        Psum = sp.coo_matrix((data, (I, J)), shape=(n_time,n)).tocsr()
 
         emin_0 = (~inds_tmp).astype(float)
-        Cv = -2*Sskv * n_equiv * dz
+        Cv = 2*Sskv * n_equiv * dz
         
         # For evaluating elastic subsidence from clays
         inds_e = np.argwhere(utils.mkvc(INDS)==True).ravel()
@@ -245,24 +260,29 @@ class InSARSimulatino1D(BaseSimulation):
         data = np.r_[diag, lower]
         data[np.in1d(I, inds_e)] = 0
         Pdiff = sp.coo_matrix((data, (I, J)), shape=(n,n))
-        Ce = - 2 * Sske * n_equiv * dz
+        Ce = 2 * Sske * n_equiv * dz
         
         # For evaluating elastic subsidence from clays
         I = np.hstack([np.arange(n_time-ii) + ii for ii in range(n_time)])
         J = np.hstack([np.ones(n_time-ii)*ii for ii in range(n_time)])
         data = np.ones_like(I)
-        Pcumsum = sp.coo_matrix((data, (I, J)), shape=(n_time,n_time))
+        # Pcumsum = sp.coo_matrix((data, (I, J)), shape=(n_time,n_time)).tocsr()
 
-        Ca = Sska * b_aquifer
+        Ca = -Sska * b_aquifer
         e = np.ones(n_time-1)
         I = np.r_[np.arange(n_time-1) + 1, np.arange(n_time-1) + 1]
         J = np.r_[np.arange(n_time-1) + 1, np.zeros(n_time-1)]
         data = np.r_[-1*np.ones(n_time-1), np.ones(n_time-1)]
-        Pa = sp.coo_matrix((data, (I, J)), shape=(n_time,n_time))
-
+        Pa = sp.coo_matrix((data, (I, J)), shape=(n_time,n_time)).tocsr()
+        # TODO: change P matricies such that it can handle band-limited delattra
+        # Psum, Pcumsum, Pa
         # Calculate subsidence of each of the three sources
         b_v = Cv * (Psum @ (Pmin @ utils.mkvc(h) + (emin_0-1) * h_min0))
-        b_e = Ce * (Pcumsum @ (Psum @ (Pdiff @ utils.mkvc(h))))
+        # b_v = Cv * (Psum @ (Pmin @ utils.mkvc(h) - h_min0))
+        # there seems correlated error with time steps when Ssk is changing
+        b_v = np.r_[0, np.cumsum(medfilt(np.diff(b_v)))]
+        # b_e = Ce * (Pcumsum @ (Psum @ (Pdiff @ utils.mkvc(h))))
+        b_e = Ce * (Psum @ (utils.mkvc(h) - Pmin @ utils.mkvc(h)))
         b_a = Ca * (Pa @ self.h_aquifer)
 
         b_t = b_v + b_e + b_a        
@@ -270,7 +290,7 @@ class InSARSimulatino1D(BaseSimulation):
         f = {}
         f['Psum'] = Psum
         f['Pmin'] = Pmin
-        f['Pcumsum'] = Pcumsum
+        # f['Pcumsum'] = Pcumsum
         f['Pdiff'] = Pdiff
         f['Pa'] = Pa
         f['emin_0'] = emin_0
@@ -285,22 +305,45 @@ class InSARSimulatino1D(BaseSimulation):
         for prop in self._clear_on_update:
             delattr(self, prop)
         return f
-
+    
     def dpred(self, m=None, f=None):
         """
-            Calculate the predicted subsidence
+        dpred(m, f=None)
+        Create the projected data from a model.
+        The fields, f, (if provided) will be used for the predicted data
+        instead of recalculating the fields (which may be expensive!).
+
+        .. math::
+
+            d_\\text{pred} = P(f(m))
+
+        Where P is a projection of the fields onto the data space.
         """
+        if self.survey is None:
+            raise AttributeError(
+                "The survey has not yet been set and is required to compute "
+                "data. Please set the survey for the simulation: "
+                "simulation.survey = survey"
+            )
+
         if f is None:
+            if m is None:
+                m = self.model
+
             f = self.fields(m)
-        
-        return f['b_t']
+
+        data = Data(self.survey)
+        for src in self.survey.source_list:
+            for rx in src.receiver_list:
+                data[src, rx] = rx.eval(self.time_mesh, f)
+        return utils.mkvc(data)
 
     # Use the perturbation method, central difference. 
     # Cost is the two forward simulations
     # Perturbation factor is hard-coded as 0.01.
     # Need to test see if they can pass the order tests. 
 
-    def get_J_Sskv(self, m, f=None):
+    def get_J_Sskv(self, m, f=None, adjoint=False):
         Sskv_current = self.Sskv
         dSskv = 0.01 * Sskv_current
         self.Sskv = Sskv_current - dSskv
@@ -311,7 +354,7 @@ class InSARSimulatino1D(BaseSimulation):
         J_Sskv = (f2['b_t']-f1['b_t']) / (2*dSskv)
         return J_Sskv.reshape([-1,1])
 
-    def get_J_Sske(self, m, f=None):
+    def get_J_Sske(self, m, f=None, adjoint=False):
         Sske_current = self.Sske
         dSske = 0.01 * Sske_current
         self.Sske = Sske_current - dSske
@@ -322,7 +365,7 @@ class InSARSimulatino1D(BaseSimulation):
         J_Sske = (f2['b_t']-f1['b_t']) / (2*dSske)        
         return J_Sske.reshape([-1,1])
 
-    def get_J_Kv(self, m, f=None):
+    def get_J_Kv(self, m, f=None, adjoint=False):
         Kv_current = self.Kv
         dKv = 0.01 * Kv_current
         self.Kv = Kv_current - dKv
@@ -333,7 +376,7 @@ class InSARSimulatino1D(BaseSimulation):
         J_Kv = (f2['b_t']-f1['b_t']) / (2*dKv)        
         return J_Kv.reshape([-1,1])
 
-    def get_J_b_equiv(self, m, f=None):
+    def get_J_b_equiv(self, m, f=None, adjoint=False):
         b_equiv_current = self.b_equiv
         db_equiv = 0.01 * b_equiv_current
         self.b_equiv = b_equiv_current - db_equiv
@@ -344,7 +387,7 @@ class InSARSimulatino1D(BaseSimulation):
         J_b_equiv = (f2['b_t']-f1['b_t']) / (2*db_equiv)        
         return J_b_equiv.reshape([-1,1])
 
-    def get_J_h_min0(self, m, f=None):
+    def get_J_h_min0(self, m, f=None, adjoint=False):
         h_min0_current = self.h_min0
         dh_min0 = 0.01 * h_min0_current
         self.h_min0 = h_min0_current - dh_min0
@@ -358,36 +401,76 @@ class InSARSimulatino1D(BaseSimulation):
         # test below idea -> it is not working, so reality is a bit more complicated.
         # return Cv * (Psum @ (emin_0-1))
 
-    def get_J_n_equiv(self, m, f=None):        
+    def get_J_n_equiv(self, m, f=None, adjoint=False):        
         if f is None:
             f = self.fields(m)
         J_n_equiv = f['b_v']/self.n_equiv + f['b_e']/self.n_equiv
         return J_n_equiv.reshape([-1,1])
     
-    def get_J_Sska(self, m, f=None):
+    def get_J_Sska(self, m, f=None, adjoint=False):
         if f is None:
             f = self.fields(m)
         Pa = f['Pa']
         J_Sska = self.b_aquifer * (Pa * self.h_aquifer)
         return J_Sska.reshape([-1,1])
 
-    def get_J_b_aquifer(self, m, f=None):
+    def get_J_b_aquifer(self, m, f=None, adjoint=False):
         Pa = f['Pa']
         J_b_aquifer = self.Sska * (Pa @ self.h_aquifer)
         return J_b_aquifer.reshape([-1,1])
 
-    def get_J_h_aquifer(self, m, f=None):
-        pass
+    def get_J_h_aquifer(self, m, f=None, adjoint=False):
+        raise Exception("Not implemented yet!")
+        if f is None:
+            f = self.fields(m, f=f)
+        return self.get_J_h_aquifer_diff(m, f=f)
+
+    def get_J_h_aquifer_diff(self, m, f=None, adjoint=False):        
+        h_aquifer_current = self.h_aquifer.copy()
+        J_h_aquifer = np.zeros((self.n_time, self.h_aquifer.size), dtype=float)
+        for ii in range(len(self.h_aquifer)):
+            h_aquifer_ii = h_aquifer_current[ii]
+            dh_aquifer_ii = 0.01 * h_aquifer_ii
+            self.h_aquifer[ii] = h_aquifer_ii - dh_aquifer_ii
+            f1 = self.fields([])
+            self.h_aquifer[ii] = h_aquifer_ii + dh_aquifer_ii
+            f2 = self.fields([])
+            J_h_aquifer[:,ii] =  (f2['b_t']-f1['b_t']) / (2*dh_aquifer_ii)     
+        self.h_aquifer = h_aquifer_current
+        return J_h_aquifer
 
     def get_J(self, m, f=None):
         pass
 
-    def Jvec(self, m, f=None):
-        pass
+    def Jvec(self, m, v, f=None):
+        # Output is nD x 1 vector
+        Jvec = np.zeros(survey.nD, dtype=float)
+        Jvec += self.get_J_Sskv(m, f=f) @ (self.SskvDeriv @ v)
+        Jvec += self.get_J_Sske(m, f=f) @ (self.SskeDeriv @ v)
+        Jvec += self.get_J_Kv(m, f=f) @ (self.KvDeriv @ v)
+        Jvec += self.get_J_h_min0(m, f=f) @ (self.h_min0Deriv @ v)
+        Jvec += self.get_J_n_equiv(m, f=f) @ (self.n_equivDeriv @ v)
+        Jvec += self.get_J_b_equiv(m, f=f) @ (self.b_equivDeriv @ v)
+        Jvec += self.get_J_Sska(m, f=f) @ (self.SskaDeriv @ v)
+        Jvec += self.get_J_b_aquifer(m, f=f) @ (self.b_aquiferDeriv @ v)
+        # May be this one could be optional for not to store J?
+        Jvec += self.get_J_h_aquifer(m, f=f) @ (self.h_aquiferDeriv @ v)
+        return Jvec
     
-    def Jtvec(self, m, f=None):
-        pass
-
+    def Jtvec(self, m, v, f=None):
+        # Output is M x 1 vector
+        Jtvec = np.zeros(m.size, dtype=float)
+        Jtvec += self.SskvDeriv.T * (self.get_J_Sskv(m, f=f, adjoint=True) @ v)
+        Jtvec += self.SskeDeriv.T * (self.get_J_Sske(m, f=f, adjoint=True) @ v)
+        Jtvec += self.KvDeriv.T * (self.get_J_Kv(m, f=f, adjoint=True) @ v)
+        Jtvec += self.h_min0Deriv.T * (self.get_J_h_min0(m, f=f, adjoint=True) @ v)
+        Jtvec += self.n_equivDeriv.T * (self.get_J_n_equiv(m, f=f, adjoint=True) @ v)
+        Jtvec += self.b_equivDeriv.T * (self.get_J_b_equiv(m, f=f, adjoint=True) @ v)
+        Jtvec += self.SskaDeriv.T * (self.get_J_Sska(m, f=f, adjoint=True) @ v)
+        Jtvec += self.b_aquiferDeriv.T * (self.get_J_b_aquifer(m, f=f, adjoint=True) @ v)
+        # May be this one could be optional for not to store J?
+        Jtvec += self.h_aquiferDeriv.T * (self.get_J_h_aquifer(m, f=f, adjoint=True) @ v)     
+        return Jtvec
 
     @property
     def _clear_on_update(self):
---
+++ insarinv/survey.py
@@ -0,0 +1,77 @@
+from SimPEG.survey import BaseTimeRx, BaseSurvey, BaseSrc
+import properties
+import numpy as np
+import scipy.sparse as sp
+
+
+####################################################
+# Survey
+####################################################
+
+
+class Survey(BaseSurvey):
+    """
+    Time domain electromagnetic survey
+    """
+
+    source_list = properties.List(
+        "A list of sources for the survey",
+        properties.Instance("A SimPEG source", BaseSrc),
+        default=[],
+    )
+
+    def __init__(self, source_list=None, **kwargs):
+        super(Survey, self).__init__(source_list, **kwargs)
+
+####################################################
+# Source
+####################################################
+
+
+class Src(BaseSrc):
+
+    def __init__(self, location, receiver_list=None, **kwargs):
+        if receiver_list is not None:
+            kwargs["receiver_list"] = receiver_list
+        super(BaseSrc, self).__init__(location=location, **kwargs)
+
+
+####################################################
+# Receiver
+####################################################
+
+class Rx(BaseTimeRx):
+    """
+    Time domain receiver base class
+
+    :param numpy.ndarray locations: receiver locations (ie. :code:`np.r_[x,y,z]`)
+    :param numpy.ndarray times: times
+    :param string orientation: receiver orientation 'x', 'y' or 'z'
+    """
+    
+    _P = None
+
+    def __init__(self, times, orientation=None, **kwargs):
+        super().__init__(times=times, **kwargs)
+
+    @property
+    def P(self):
+        return self._P
+
+    @property
+    def nD(self):
+        return self.times.size
+
+    def eval(self, time_mesh, f):
+        
+        if self._P is None:
+            P = self.getTimeP(time_mesh)
+            n_data = self.times.size
+            tmp = np.arange(n_data-1) + 1
+            I = np.r_[tmp, tmp].astype(int)
+            J = np.r_[tmp, np.zeros(n_data-1)].astype(int)
+            data = np.r_[np.ones(n_data-1), -np.ones(n_data-1)]
+            Prel = sp.coo_matrix((data, (I, J)), shape=(n_data, n_data))        
+            self._P = Prel * P
+
+        return self.P * f['b_t']
